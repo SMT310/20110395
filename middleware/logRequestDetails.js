f2be1c103d61a0d243dff1dd6992412252c7c3da
SMT310 <shinizeam@gmail.com>
2023-10-03 23:06:13 +0700

    fixed console.log of loggers

--- middleware/logRequestDetails.js
+++ middleware/logRequestDetails.js
@@ -2,7 +2,7 @@ const logRequestDetails = (req, res, next) => {
     const method = req.method;
     const url = req.originalUrl || req.url;
 
-    console.log(`Request Method: ${method}, URL: ${url}`);
+    console.log(`Request Method: [${method}], URL: ${url}`);
     next();
 };
 
