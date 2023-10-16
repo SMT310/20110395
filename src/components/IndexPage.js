54c655ab3a1f59c7ea9c85a91d077089800f000f
SMT310 <shinizeam@gmail.com>
2023-10-16 21:33:37 +0700

    complete week6

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -25,7 +25,7 @@ function PostCard({ post, onDeletePost }) {
             }}>
             <CardMedia
                 sx={{ height: 240 }}
-                image='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1729749d-144e-4191-b39e-416758fa0e86/dfl7vfc-b3b78b41-0e04-4af6-bf8a-280a8d896cf5.png/v1/fill/w_1280,h_800/one_piece_strong_world__roronoa_zoro_wallpaper_by_shinreidulv_dfl7vfc-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvMTcyOTc0OWQtMTQ0ZS00MTkxLWIzOWUtNDE2NzU4ZmEwZTg2XC9kZmw3dmZjLWIzYjc4YjQxLTBlMDQtNGFmNi1iZjhhLTI4MGE4ZDg5NmNmNS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.e268QptKOgbnrTMlJsCmY75ylT4CLDjBYxwiSIQl63A'
+                image='https://m.media-amazon.com/images/M/MV5BYWFhOGQwODctMDFmNC00NGEyLWI5NTUtZDliYzRmYmM4MzJlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_.jpg'
                 title={post.title}
             />
             <CardContent>
